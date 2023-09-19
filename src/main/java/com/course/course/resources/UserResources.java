3eabc83caa8f0312469570cd4e51eb5187ea3f56
bruno <bruno.cabral.silva2018@gmail.com>
2023-09-19 15:38:29 -0300

    Criação de endpoints para findAll e findById para o componente User

--- src/main/java/com/course/course/resources/UserResources.java
+++ src/main/java/com/course/course/resources/UserResources.java
@@ -1,24 +1,32 @@
 package com.course.course.resources;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.course.course.entities.User;
+import com.course.course.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
-  
+  @Autowired
+  private UserService userService;
+
   @GetMapping
-  public ResponseEntity<User> findAll() {
-    User user = new User();
-    user.setName("bruno");
-    user.setEmail("bruno@gmail.com");
-    user.setPhone("+55 83 998364408");
-    user.setPassword("123456");
+  public ResponseEntity<List<User>> findAll() {
+    List<User> list = userService.findAll();
+    return ResponseEntity.ok().body(list);
+  };
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<User> findById(@PathVariable Long id) {
+    User user = userService.findById(id);
     return ResponseEntity.ok().body(user);
   };
 }
